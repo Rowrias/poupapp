3fbcf28c7fde3d843705a34169877b294061f685
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-10-09 14:52:49 -0300

    02.Utilizando o Tailwind
    - Criando a barra lateral
    - Importando a fonte
    - Criando o container principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import logo from './assets/logo.svg'
+import { Aside } from "./components/Aside"
+import { Container } from "./components/Container"
 
 function App() {
 
   return (
     <div className="bg-neutral-background min-h-screen">
-      <aside>
-        <img src={logo} alt="logo" />
-      </aside>
+      <Container>
+        <Aside />
+      </Container>
     </div>
   )
 }
