32ad2348fee03c1fe4bb4e879399a4bc0a5f784a
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-10-10 09:16:25 -0300

    02.Utilizando o Tailwind
    - Implementando input de texto e Main
    - Desenvolvendo componente para tipografia
    - Estruturando o card, card.header e card.body
    - Estilizando o componente de orçamento diário

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { Aside } from "./components/Aside"
+import Card, { CardBody } from "./components/Card"
 import { Container } from "./components/Container"
+import { DailyBudget } from "./components/DailyBudget"
+import { Main } from "./components/Main"
+import { SearchInput } from "./components/SearchInput"
+import { Typography } from "./components/Typography"
+import { SavingsStatus } from "./SavingsStatus"
 
 function App() {
 
@@ -7,6 +13,52 @@ function App() {
     <div className="bg-neutral-background min-h-screen">
       <Container>
         <Aside />
+        <Main>
+          <SearchInput name="q" />
+          <div>
+            <Typography variant="h1">
+              Olá, Vinny
+            </Typography>
+            <Typography>
+              Veja como estão suas finanças hoje.
+            </Typography>
+          </div>
+          <section>
+            <Card>
+              <Card.Header>
+                Orçamento diário disponível:
+              </Card.Header>
+              <CardBody>
+                <DailyBudget value={42} />
+              </CardBody>
+            </Card>
+            <Card>
+              <Card.Header>
+                Progresso da meta financeira
+              </Card.Header>
+              <CardBody>
+                <SavingsStatus />
+              </CardBody>
+            </Card>
+            <Card>
+              <Card.Header>
+                Movimentação financeira
+              </Card.Header>
+              <CardBody>
+                R$ 200
+              </CardBody>
+            </Card>
+            <Card>
+              <Card.Header>
+                Minhas contas
+              </Card.Header>
+              <CardBody>
+                R$ 200
+              </CardBody>
+            </Card>
+          </section>
+        </Main>
+
       </Container>
     </div>
   )
