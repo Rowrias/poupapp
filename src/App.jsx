a93833420ce328f313dbc00ef2fee4f609685b5c
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-11-11 09:54:08 -0300

    02.Utilizando o Tailwind
    - Implementando barra de progresso
    - Montando o grid para os Cards
    - Desafio

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
+import { Accounts } from "./components/Accounts"
 import { Aside } from "./components/Aside"
-import Card, { CardBody } from "./components/Card"
+import Card from "./components/Card"
 import { Container } from "./components/Container"
 import { DailyBudget } from "./components/DailyBudget"
 import { Main } from "./components/Main"
+import { SavingsStatus } from "./components/SavingsStatus"
 import { SearchInput } from "./components/SearchInput"
+import { Transactions } from "./components/Transactions" 
 import { Typography } from "./components/Typography"
-import { SavingsStatus } from "./SavingsStatus"
 
 function App() {
 
   return (
     <div className="bg-neutral-background min-h-screen">
       <Container>
+
         <Aside />
+        
         <Main>
           <SearchInput name="q" />
           <div>
@@ -23,38 +27,38 @@ function App() {
               Veja como estão suas finanças hoje.
             </Typography>
           </div>
-          <section>
+          <section className="grid grid-cols-2 items-stretch gap-6">
             <Card>
               <Card.Header>
                 Orçamento diário disponível:
               </Card.Header>
-              <CardBody>
+              <Card.Body>
                 <DailyBudget value={42} />
-              </CardBody>
+              </Card.Body>
             </Card>
             <Card>
               <Card.Header>
                 Progresso da meta financeira
               </Card.Header>
-              <CardBody>
+              <Card.Body>
                 <SavingsStatus />
-              </CardBody>
+              </Card.Body>
             </Card>
             <Card>
               <Card.Header>
                 Movimentação financeira
               </Card.Header>
-              <CardBody>
-                R$ 200
-              </CardBody>
+              <Card.Body>
+                <Transactions />
+              </Card.Body>
             </Card>
             <Card>
               <Card.Header>
                 Minhas contas
               </Card.Header>
-              <CardBody>
-                R$ 200
-              </CardBody>
+              <Card.Body>
+                <Accounts />
+              </Card.Body>
             </Card>
           </section>
         </Main>
